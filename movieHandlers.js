2c48bd574c039aee3067db9c9fb441c030424f73
Leo Terrier <leoterrier22@gmail.com>
2022-11-10 09:38:46 +0100

    sixth commit

--- movieHandlers.js
+++ movieHandlers.js
@@ -160,6 +160,25 @@ const putUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -169,4 +188,5 @@ module.exports = {
   postUser,
   putMovie,
   putUser,
+  deleteMovie
 };
